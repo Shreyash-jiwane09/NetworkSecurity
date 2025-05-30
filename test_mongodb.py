3c5ebf744856189e09e2e98dfc3e2a3f3b8df128
Shreyash-jiwane09 <jiwane.shrey09@gmail.com>
2025-05-30 12:39:52 +0530

    Changed MONGODB url

--- test_mongodb.py
+++ test_mongodb.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://shreyashjiwane:Shrey0902@cluster0.kggpgvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://shreyashjiwane:jiwane1997@cluster0.kggpgvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
